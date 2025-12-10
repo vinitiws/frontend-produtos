6522fbad82a901fb95bb09b8960c5308b39e22f3
vinicius <viniciusnicacio80@gmail.com>
2025-12-10 16:58:16 -0300

    tabele e formulario adicionados

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Formulario from './Formulario';
+import Tabela from './Tabela';
 
 function App() {
+  // UseState para controlar o estado do botão
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Formulario botao={btnCadastrar} />
+      <Tabela />
     </div>
   );
 }
---
+++ src/Formulario.js
@@ -0,0 +1,23 @@
+
+function Formulario({ botao }) {
+
+    return (
+        <form>
+            <input type="text" placeholder="Nome" className="form-control" />
+            <input type="text" placeholder="Marca" className="form-control" />
+
+            {botao
+                ?
+                <input type="button" value='Cadastrar' className="btn btn-primary" />
+                :
+                <div>
+                    <input type="button" value='Alterar' className="btn btn-warning" />
+                    <input type="button" value='Remover' className="btn btn-danger" />
+                    <input type="button" value='Cancelar' className="btn btn-secondary" />
+                </div>
+            }
+        </form>
+
+    )
+}
+export default Formulario;
